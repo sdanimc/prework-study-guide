a746dc7ff90fedabb88ded1f9568a4f8ac6e9366
sdanimc <116037382+sdanimc@users.noreply.github.com>
2022-10-24 13:55:24 -0700

    added more js code

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,26 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
-for (var x = 0; x < topics.length; x++) {
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
+
+function listTopics () {
+    for (var x = 0; x < topics.length; x++) {
     console.log(topics[x]);
+    }
 }
-/*
-if (topic === 'HTML'){
+
+function selectTopic() {
+    if (randomTopic === 'HTML'){
     console.log("let's study html!");
-} else if (topic === 'CSS') {
+} else if (randomTopic === 'CSS') {
     console.log("let's study CSS!");
-} else if (topic === 'Git') {
+} else if (randomTopic === 'Git') {
     console.log("let's study Git!");
-} else if (topic === 'JavaScript') {
+} else if (randomTopic === 'JavaScript') {
     console.log("let's study JavaScript!");
 } else {
     console.log("Please try again")
 }
-*/
+}
+console.log('Here are topics we learned through Prework');
+listTopics()
+console.log('which topic should we chose?');
+selectTopic()
