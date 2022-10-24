6ef9e667263c94826e37a8838994c7e105834afe
sdanimc <116037382+sdanimc@users.noreply.github.com>
2022-10-24 12:37:41 -0700

    added javascript

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+for (var x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+}
+/*
+if (topic === 'HTML'){
+    console.log("let's study html!");
+} else if (topic === 'CSS') {
+    console.log("let's study CSS!");
+} else if (topic === 'Git') {
+    console.log("let's study Git!");
+} else if (topic === 'JavaScript') {
+    console.log("let's study JavaScript!");
+} else {
+    console.log("Please try again")
+}
+*/
